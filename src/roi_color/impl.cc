4a360397578cf2f5555d781c0c126ce9b6856ecc
XunYang818 <13042725395@163.com>
2024-11-15 02:50:45 +0800

    Update impl.cc

--- src/roi_color/impl.cc
+++ src/roi_color/impl.cc
@@ -31,25 +31,24 @@ std::unordered_map<int, cv::Rect> roi_color(const cv::Mat& input) {
     std::unordered_map<int, cv::Rect> res;
     // IMPLEMENT YOUR CODE HERE
     std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
     cv::Mat gray_image;
     cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
     cv::Mat binary_image;
     cv::threshold(gray_image, binary_image, 0, 255, cv::THRESH_BINARY_INV + cv::THRESH_OTSU);
-    cv::findContours(binary_image, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    cv::findContours(binary_image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
     for (size_t i = 0; i < contours.size(); i++)
     {
         cv::Rect rect = cv::boundingRect(contours[i]);
         cv::Mat roi = input(rect);
         int blue = 0, green = 0, red = 0;
-        for (int y = 0; y < roi.rows; y++)
+        for (int x = 0; x < roi.cols; x++)
+        {
+            for (int y = 0; y < roi.rows; y++)
             {
-            for (int x = 0; x < roi.cols; x++)
-            {
-                cv::Vec3b color = roi.at<cv::Vec3b>(y, x);
-                red += color[2];
-                green += color[1];
+                cv::Vec3b color = roi.at<cv::Vec3b>(x, y);
                 blue += color[0];
+                green += color[1];
+                red += color[2];
             }
         }
         int totalPixels = roi.rows * roi.cols;
