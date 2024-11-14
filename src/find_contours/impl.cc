c3dd005cc84041c5c500ebf29b5e9473f5a0cb08
XunYang818 <13042725395@163.com>
2024-11-15 01:37:15 +0800

    Update impl.cc

--- src/find_contours/impl.cc
+++ src/find_contours/impl.cc
@@ -17,13 +17,24 @@ std::vector<std::vector<cv::Point>> find_contours(const cv::Mat& input) {
      * 运行测试点，你找到的轮廓与答案的轮廓一样就行。
      */
     
-    std::vector<std::vector<cv::Point>> res;
+    std::vector<std::vector<cv::Point>> contours;
     // IMPLEMENT YOUR CODE HERE
     std::vector<cv::Vec4i> hierarchy;
     cv::Mat gray_image;
     cv::cvtColor(input, gray_image, cv::COLOR_BGR2GRAY);
     cv::Mat binary_image;
     cv::threshold(gray_image, binary_image, 100, 255, cv::THRESH_BINARY);
-    cv::findContours(binary_image, res, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    return res;
+    cv::findContours(binary_image, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+    // 过滤出最内层轮廓
+    std::vector<std::vector<cv::Point>> inner_contours;
+    for (size_t i = 0; i < hierarchy.size(); i++)
+    {
+        // 如果当前轮廓没有子轮廓，则认为是内层轮廓
+        if (hierarchy[i][2] == -1)
+        {
+            inner_contours.push_back(contours[i]);
+        }
+    }
+
+    return inner_contours;
 }
